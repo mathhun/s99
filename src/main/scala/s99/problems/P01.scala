e694613917b9d02f872a82ca8507f3cbc69c5f34
mathhun <achemer@gmail.com>
2016-04-18 18:14:34 +0900

    P01.scala

--- src/main/scala/s99/problems/P01.scala
+++ src/main/scala/s99/problems/P01.scala
@@ -1,5 +1,11 @@
 package s99.problems
 
 object P01 {
-  def last[A](xs: List[A]): A = ???
+  def last[A](xs: List[A]): A = {
+    if (xs.isEmpty) throw new Exception("list is empty")
+    else xs match {
+      case x :: Nil => x
+      case x :: xs => last(xs)
+    }
+  }
 }
